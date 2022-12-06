90f9e6bde365e70e0246e5617c78d6f03348f1d1
pius ssekweyama <ssekweyamapius@gmail.com>
2022-12-06 10:50:07 +0300

    display list of items on the home page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
 import $ from 'jquery';
 import animate from './modules/animate.js';
+import load from './modules/meals.js';
 
 $(document).ready(($) => {
   animate($);
+  load();
 });
