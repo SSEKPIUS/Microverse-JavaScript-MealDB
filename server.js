ccc14dc266b6508028dce8deb22dfce7e22875e8
pius ssekweyama <ssekweyamapius@gmail.com>
2022-12-06 08:29:59 +0300

    added footer

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(
     publicPath: config.output.publicPath,
   }),
 );
-
-// Serve the files on port 3000.
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!\n');
 });
