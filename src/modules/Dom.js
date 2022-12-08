4b2c98dcff9ea6fa3c09a4736d9006f6930d4be4
Precious Nhlapho <pnhlapho59@gmail.com>
2022-12-08 10:29:08 +0200

    Update comment section

--- src/modules/Dom.js
+++ src/modules/Dom.js
@@ -1,16 +1,18 @@
 import ht2 from './images/heart2.png';
 import countFoods from './countFoods.js';
 import { addlikes, getlikes } from './likes.js';
-import showPopup from './popup.js';
+import { showPopup } from './popup.js';
 
-const container = document.querySelector('.food-container');
-const popup = document.querySelector('#modalDisplay');
+const container = document.querySelector('.container');
+
 const loadPopUp = (id) => {
   showPopup(id);
 };
+
 const like = (element) => {
   addlikes(element);
 };
+
 const innerData = (meals) => {
   const holder = document.createElement('div');
   holder.classList.add('grid-container');
@@ -21,13 +23,13 @@ const innerData = (meals) => {
           <div class="food">
               <div>
                  <span style="display: none" id="idCategory">${meal.idMeal}</span>
-                  <img src="${meal.strMealThumb}" alt="">
+                  <img src="${meal.strMealThumb}" height="200" alt="">
               </div>
-              <div class="food-name padded">
+              <div class="food-name">
                   <span>${meal.strMeal}</span>
                   <span class="like"><img src="${ht2}" alt="" width="15" height="15" srcset=""></span>
               </div>
-              <div class="likes padded">
+              <div class="likes">
                   <span class="likes-count">0<span> likes</span></span>
               </div>
               <div>
@@ -36,47 +38,7 @@ const innerData = (meals) => {
           </div>
       </div>`;
     div.querySelector('input[type="button"]').addEventListener('click', () => {
-      // loadPopUp(meal.idMeal);
-
-      const pop = () => {
-        popup.innerHTML = `
-          <div class="popIt">
-            <div class ="flex pop column">
-             <span class="close-button">&times;</span>
-             <div class="popup_image">
-                <img class="pop_image" src="${meal.strMealThumb}" alt="asdf">
-              </div>
-              <p class="pop_meal">Name: ${meal.strMeal}</p> 
-            
-              <div class="comment_count">
-              </div>
-              
-              <div class="pop_comment">
-              </div>
-              <div id ="commentList"></div>
-                <h3 class="add-comm">Add a comment</h3>
-              <div class="inputDiv">
-                <form class="form">
-                  <input id="name"  type="text" name="user" required placeholder="Your Name"><br>
-                  <textarea id="text" type="text" rows=6 name="text" required placeholder="Your insight"></textarea><br>
-                  <div>
-                  <button class="submit-btn" type="submit">Comment</button>
-                  <button class="view_more">Refresh Comments</button>
-                </div>
-                </form>
-                
-              </div>
-            </div>
-            </div>`;
-        popup.style.display = 'flex';
-        document.body.style.overflow = 'hidden';
-      };
-      pop();
-      const close = document.querySelector('.close-button');
-      close.addEventListener('click', () => {
-        popup.style.display = 'none';
-        document.body.style.overflow = 'auto';
-      });
+      loadPopUp(meal.idMeal);
     });
     div.querySelector('.like').addEventListener('click', (e) => {
       like(e.target.parentNode.parentNode.parentNode.querySelector('#idCategory').innerText);
@@ -85,9 +47,11 @@ const innerData = (meals) => {
   });
   return holder;
 };
+
 const fillDom = (meals) => {
   container.append(innerData(meals));
   countFoods(meals.length);
   getlikes();
 };
+
 export { fillDom, innerData };
