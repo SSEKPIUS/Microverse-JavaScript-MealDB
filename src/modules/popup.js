a007adafeef6a8ab5598cd54c5d652ea68a111d7
Precious Nhlapho <pnhlapho59@gmail.com>
2022-12-08 10:35:44 +0200

    Add comment counter

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,79 +1,58 @@
-import { generateComment, addNewComment } from './comment.js';
+export const getComments = async (id) => {
+  const res = await fetch(`${process.env.BaseURL}comments?item_id=${id}`);
+  const data = await res.json();
 
-export const fetchMealData = async (id) => {
-  const mealDbApi = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
-  const response = await fetch(`${mealDbApi}`);
-  const data = await response.json();
-  return data.meals;
+  if (!res.ok) {
+    return null;
+  }
+  return data;
 };
 
-const addCommentEvent = async () => {
-  const form = document.querySelector('#form');
-  const addCommentBtn = document.querySelector('.add-comment-btn');
+// eslint-disable-next-line
+const createCommentHtml = ({ creation_date, username, comment }) => {
 
-  form.addEventListener('submit', async (ev) => {
-    ev.preventDefault();
-    const user = form.elements.fname;
-    const comment = form.elements.newUserComment;
-
-    await addNewComment(addCommentBtn.id, user.value, comment.value);
-
-    const commentDiv = document.querySelector('.comments-div');
-    const commentParagraph = await generateComment(addCommentBtn.id);
-    commentDiv.innerHTML = '';
-    commentDiv.append(commentParagraph);
-
-    form.reset();
-  });
-};
-
-const addCloseEvent = () => {
-  const closeBtn = document.getElementById('closebtn');
-  closeBtn.addEventListener('click', () => {
-    const popupSection = document.querySelector('.pop-up');
-    popupSection.style.display = 'none';
-  });
-};
-
-const createPopHtml = (mealDetails) => {
-  const popupDetails = document.createElement('div');
-  popupDetails.className = 'popup-details';
-  popupDetails.innerHTML = `
-    <span id="closebtn" class="closebtn">&times;</span>
-            <div class="meal">
-               <img src='${mealDetails.strMealThumb}' alt="meal">
-            </div>
-            <h2>${mealDetails.strMeal}</h2>
-            <div class="flex">
-                <h4>Area: ${mealDetails.strArea}</h4>
-                <h4>Category: ${mealDetails.strCategory}</h4>
-            </div>
-            <h3>Comments (<span id="comment-counter"></span>)</h3>
-            <div class="comments-div">
-              
-            </div>
-            <h3>Add a Comment</h3>
-            <form method="post" class="flex" id="form">
-                <input class="name-field" type="text" id="fname" name="fname" value="" placeholder="Your name" required>
-                <textarea maxlength="500" cols="30" rows="10" id="newUserComment" class="text-area" placeholder="Your insights" required></textarea>
-                <button class="add-comment-btn" id='${mealDetails.idMeal}' type="submit">Comment</button>
-              </form> 
+  // eslint-disable-next-line
+    let creationDate = creation_date
+  const commentSpan = document.createElement('span');
+  commentSpan.className = 'comment-sp';
+  commentSpan.innerText = `
+      ${creationDate} ${username}: ${comment}  
     `;
-  return popupDetails;
+  return commentSpan;
 };
 
-export const showPopup = async (id) => {
-  const popupSection = document.querySelector('.pop-up');
-  popupSection.innerHTML = ' ';
+export const generateComment = async (id) => {
+  const commentParagraph = document.createElement('p');
+  commentParagraph.innerHTML = '';
 
-  const mealDetails = await fetchMealData(id);
-  popupSection.append(createPopHtml(mealDetails[0]));
+  const comments = await getComments(id);
+  if (comments == null) {
+    const emptyCommentSpan = document.createElement('span');
+    emptyCommentSpan.className = 'no-comment';
+    emptyCommentSpan.textContent = 'be the first to comment';
+    commentParagraph.append(emptyCommentSpan);
+  } else {
+    comments.forEach((comment) => {
+      commentParagraph.append(createCommentHtml(comment));
+    });
+  }
 
-  const commentDiv = document.querySelector('.comments-div');
-  const commentParagraph = await generateComment(id);
-  commentDiv.append(commentParagraph);
+  return commentParagraph;
+};
 
-  popupSection.style.display = 'block';
-  addCommentEvent();
-  addCloseEvent();
+export const addNewComment = async (id, user, comment) => {
+  const newComment = {
+    item_id: id,
+    username: user,
+    comment,
+  };
+  const res = await fetch(`${process.env.BaseURL}comments`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newComment),
+  });
+
+  return res;
 };
